057809ba53f1d8234a083ad7f9d33161095e9643
Rainer Simon <rainer@rainersimon.io>
2025-02-20 10:43:33 +0100

    Fixed Supabase warning about stale user object

--- src/sdk/profile/get-my-profile.ts
+++ src/sdk/profile/get-my-profile.ts
@@ -1,4 +1,4 @@
-import type { AuthError, PostgrestError, SupabaseClient } from '@supabase/supabase-js';
+import type { AuthError, PostgrestError, SupabaseClient, User } from '@supabase/supabase-js';
 import type { MyProfile } from '../../core';
 
 const SELECT_PROFILE = `
@@ -19,41 +19,65 @@ const SELECT_ORG_GROUPS = `
   description,
   is_admin`;
 
+/** 
+ * Supabase recommends against using auth.getSession() to grab the user. It will raise the 
+ * following warning:
+ * 
+ *   "Using the user object as returned from supabase.auth.getSession() or from some 
+ *   supabase.auth.onAuthStateChange() events could be insecure! This value comes directly 
+ *   from the storage medium (usually cookies on the server) and may not be authentic. 
+ *   Use supabase.auth.getUser() instead which authenticates the data by contacting the 
+ *   Supabase Auth server."
+ * 
+ * Therfore, we use this helper function.
+ */
+const getUser = (supabase: SupabaseClient): Promise<User> =>
+  supabase.auth.getSession().then(({ error, data }) => {
+    if (error || !data?.session) {
+      throw error || 'Unauthorized';
+    } else {
+      return supabase.auth.getUser().then(({ error, data }) => {
+        if (error) {
+          throw error;
+        } else if (!data.user) {
+          throw 'Unauthorized';
+        } else if (data.user.role !== 'authenticated') {
+          throw 'Unauthorized';
+        } else {
+          return data.user;
+        }
+      });
+    }
+  });
+
 export const getMyProfile = (
   supabase: SupabaseClient
 ) => (): PromiseLike<{ error: AuthError | PostgrestError | null, data: MyProfile }> =>
-  supabase.auth.getSession().then(({ error, data }) => {
-    if (error || !data?.session?.user) {
-      return { error, data: undefined as unknown as MyProfile };
-    } else {
-      const { user } = data.session;
+  getUser(supabase).then(user =>
+    supabase
+      .from('profiles')
+      .select(SELECT_PROFILE)
+      .eq('id', user.id)
+      .eq('group_users.group_type', 'organization')
+      .single()
+      .then(({ error, data }) => {
+        if (error || !data) return { error, data: undefined as unknown as MyProfile };
 
-      return supabase
-        .from('profiles')
-        .select(SELECT_PROFILE)
-        .eq('id', user.id)
-        .eq('group_users.group_type', 'organization')
-        .single()
-        .then(({ error, data }) => {
-          if (error || !data) return { error, data: undefined as unknown as MyProfile };
+        // Keep profile fields + add email
+        const { group_users, ...profile } = { ...data, email: user.email };
 
-          // Keep profile fields + add email
-          const { group_users, ...profile } = { ...data, email: user.email };
+        return supabase 
+          .from('organization_groups')
+          .select(SELECT_ORG_GROUPS)
+          .in('id', data.group_users.map(r => r.type_id))
+          .then(({ error , data }) => {
+            if (error || !data) return { error, data: undefined as unknown as MyProfile };
 
-          return supabase 
-            .from('organization_groups')
-            .select(SELECT_ORG_GROUPS)
-            .in('id', data.group_users.map(r => r.type_id))
-            .then(({ error , data }) => {
-              if (error || !data) return { error, data: undefined as unknown as MyProfile };
+            const myProfile = {
+              ...profile,
+              isOrgAdmin: data.some((r) => r.is_admin === true),
+            } as MyProfile;
 
-              const myProfile = {
-                ...profile,
-                isOrgAdmin: data.some((r) => r.is_admin === true),
-              } as MyProfile;
-
-              return { error, data: myProfile };
-            });
-        })
-    }
-  });
+            return { error, data: myProfile };
+          });
+      }));
