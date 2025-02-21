bfdfdc224483c183a217aa709b588de9ee3ec198
Rainer Simon <rainer@rainersimon.io>
2025-02-21 14:12:02 +0100

    TagAutoSuggestExtension interface

--- src/core/ExtensionComponent.ts
+++ src/core/ExtensionComponent.ts
@@ -1,5 +1,6 @@
 import type { Extension } from './Extension';
 import type { Plugin } from './Plugin';
+import type { VocabularyTerm } from './VocabularyTerm';
 
 export interface ExtensionComponentProps<T extends unknown = any> {
 
@@ -11,8 +12,22 @@ export interface ExtensionComponentProps<T extends unknown = any> {
 
 }
 
-export interface AdminExtensionComponentProps<T extends unknown = any> extends ExtensionComponentProps<T> {
+export interface AdminExtensionExtensionProps<T extends unknown = any> extends ExtensionComponentProps<T> {
 
   onChangeUserSettings(settings: any): void;
+
+}
+
+export interface TagAutosuggestExtensionProps<T extends unknown = any> extends ExtensionComponentProps<T> {
+
+  autoFocus?: boolean;
+
+  autoSize?: boolean;
+
+  value?: VocabularyTerm;
+
+  onChange(term: VocabularyTerm): void;
+
+  onSubmit(term: VocabularyTerm): void;
 
 }
