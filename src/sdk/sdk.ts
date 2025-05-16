97cf30bccb369b621a62575518d0274f9847d78d
lorinjameson <lwjameson@gmail.com>
2025-05-16 11:43:16 -0400

    Updates need for NER plugin

--- src/sdk/sdk.ts
+++ src/sdk/sdk.ts
@@ -1,58 +1,67 @@
-import { createSupabaseServerClient } from './supabase-server-client';
-import { createSupabaseBrowserClient } from './supabase-browser-client';
-import type { SupabaseClient } from '@supabase/supabase-js';
-import { getAnnotations } from './annotations';
-import { getMyProfile } from './profile';
-import { hasSelectPermissions } from './project';
-import { 
-  getDocumentLayersInContext, 
-  getDocumentLayersInProject, 
-  getLayersInContext, 
-  getLayersInProject 
-} from './layers';
+import { createSupabaseServerClient } from "./supabase-server-client";
+import { createSupabaseBrowserClient } from "./supabase-browser-client";
+import type { SupabaseClient } from "@supabase/supabase-js";
+import { getAnnotations } from "./annotations";
+import { getMyProfile } from "./profile";
+import { hasSelectPermissions } from "./project";
+import {
+  getDocumentLayersInContext,
+  getDocumentLayersInProject,
+  getLayersInContext,
+  getLayersInProject,
+} from "./layers";
+import { getDocument } from "./documents";
 
 const createSDK = (supabase: SupabaseClient) => ({
   annotations: {
-    get: getAnnotations(supabase)
+    get: getAnnotations(supabase),
   },
 
   layers: {
     getLayersInContext: getLayersInContext(supabase),
     getLayersInProject: getLayersInProject(supabase),
     getDocumentLayersInContext: getDocumentLayersInContext(supabase),
-    getDocumentLayersInProject: getDocumentLayersInProject(supabase)
+    getDocumentLayersInProject: getDocumentLayersInProject(supabase),
   },
 
   profile: {
-    getMyProfile: getMyProfile(supabase)
+    getMyProfile: getMyProfile(supabase),
   },
 
   project: {
-    hasSelectPermissions: hasSelectPermissions(supabase)
-  }
+    hasSelectPermissions: hasSelectPermissions(supabase),
+  },
+
+  document: {
+    getDocument: getDocument(supabase),
+  },
+
+  supabase,
 });
 
 const parseEnv = (env: ImportMetaEnv) => {
-  const { SUPABASE_SERVERCLIENT_URL, PUBLIC_SUPABASE, PUBLIC_SUPABASE_API_KEY } = env;
+  const {
+    SUPABASE_SERVERCLIENT_URL,
+    PUBLIC_SUPABASE,
+    PUBLIC_SUPABASE_API_KEY,
+  } = env;
   return {
     supabaseServerUrl: SUPABASE_SERVERCLIENT_URL || PUBLIC_SUPABASE,
-    supabaseAPIKey: PUBLIC_SUPABASE_API_KEY
+    supabaseAPIKey: PUBLIC_SUPABASE_API_KEY,
   };
-}
+};
 
 export const createServerSDK = (
-  request: Request, 
-  cookies: any, 
+  request: Request,
+  cookies: any,
   env: ImportMetaEnv
 ) => {
   const supabase = createSupabaseServerClient(request, cookies, parseEnv(env));
-  return supabase.then(supabase => createSDK(supabase));
-}
+  return supabase.then((supabase) => createSDK(supabase));
+};
 
-export const createBrowserSDK = (
-  env: ImportMetaEnv
-) => {
+export const createBrowserSDK = (env: ImportMetaEnv) => {
   const supabase = createSupabaseBrowserClient(parseEnv(env));
   if (!supabase) return;
   return createSDK(supabase);
-}
+};
--- src/sdk/supabase-browser-client.ts
+++ src/sdk/supabase-browser-client.ts
@@ -1,14 +1,13 @@
-import { type SupabaseClient } from '@supabase/supabase-js';
-import { createBrowserClient } from '@supabase/ssr';
+import { type SupabaseClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
 // Not ideal but prevents the browser client from being generated
 // on the server
 // @ts-ignore
-export const createSupabaseBrowserClient = (
-config: {
+export const createSupabaseBrowserClient = (config: {
   supabaseServerUrl: string;
   supabaseAPIKey: string;
 }): SupabaseClient | undefined =>
-  typeof window !== 'undefined' 
-    ? undefined 
+  typeof window === "undefined"
+    ? undefined
     : createBrowserClient(config.supabaseServerUrl, config.supabaseAPIKey);
