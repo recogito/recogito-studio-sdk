2c3d701c6561b3a5d9383ab46d936c9f2f8a26ff
Rainer Simon <rainer@rainersimon.io>
2025-09-12 10:03:31 +0200

    TS fixes

--- src/sdk/supabase-browser-client.ts
+++ src/sdk/supabase-browser-client.ts
@@ -1,4 +1,4 @@
-import { type SupabaseClient } from '@supabase/supabase-js';
+import type { SupabaseClient } from '@supabase/supabase-js';
 import { createBrowserClient } from '@supabase/ssr';
 
 // Not ideal but prevents the browser client from being generated on the server
