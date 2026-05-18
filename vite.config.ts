5bd3c8bf5cdc2bfc8bc1256d63d2872c77bedafa
Ben Silverman <ben@performantsoftware.com>
2026-05-18 15:32:07 -0400

    Replace legacy call with modern emitAssets

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [dts()],
   build: {
     ssr: true,
-    ssrEmitAssets: true,
+    emitAssets: true,
     target: 'node16',
     cssCodeSplit: false,
     lib: {
